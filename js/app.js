c8d5613142a2e9f256ea3abf95eb70eac7f6c5c1
ELiBuker <ace_e303@e303-s16.lab.lagcc>
2017-06-09 16:45:06 -0400

    added close functionality

--- js/app.js
+++ js/app.js
@@ -1,30 +1,33 @@
 var userSubmit = document.querySelector("#user-submit");
 
+var userInput = document.querySelector("#user-input");	
+
+var toDoList = document.querySelector("#to-do-list");
+
 function addToDo(event){
 	event.preventDefault();		//prevents refreshing
-
-	var userInput = document.querySelector("#user-input");	
-
-	var toDoList = document.querySelector("#to-do-list");	
-
 
 	if(userInput.value === ""){
 		return false;
 	}
 
-	//toDoList.innerHTML += "<li>" + userInput.value + "</li>"; //creates list item element on the html file
-
+	toDoList.innerHTML = "<li><i class=\"fa fa-window-close close-to-do\" aria-hidden=\"true\"></i>" + userInput.value + "</li>" + toDoList.innerHTML; //creates list item element on the html file
+	/*
 	var li = document.createElement("li");
 	var icon = document.createElement("i");
 	li.innerHTML = userInput.value + "  <i class=\"fa fa-window-close\" aria-hidden=\"true\"></i>"; 
 	toDoList.prepend(li);
-
+	*/
 	userInput.value = ""; //clears out the input box
 }
 
-function clearToDo(event){
-	event.preventDefault();
-	li.userInput.removeChild(li);
+function removeToDo(event){
+	if (event.target.classList.contains('close-to-do')){
+		var li = event.target.parentElement;
+		toDoList.removeChild(li);
+	}
 }
+
+toDoList.addEventListener("click", removeToDo, false);
 
 userSubmit.addEventListener("click", addToDo, false);
